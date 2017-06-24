a8fef727290e6812adc6a34cac4367e38cbfb4c3
licode <lili@bnl.gov>
2017-06-24 17:53:45 -0400

    DEV: number version of 112 and 113

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -45,13 +45,6 @@ source run_conda_forge_build_setup
 
 # Embarking on 6 case(s).
     set -x
-    export CONDA_NPY=111
-    export CONDA_PY=27
-    set +x
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-
-    set -x
     export CONDA_NPY=112
     export CONDA_PY=27
     set +x
@@ -59,8 +52,8 @@ source run_conda_forge_build_setup
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
-    export CONDA_NPY=111
-    export CONDA_PY=35
+    export CONDA_NPY=113
+    export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
@@ -73,14 +66,21 @@ source run_conda_forge_build_setup
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
-    export CONDA_NPY=111
+    export CONDA_NPY=113
+    export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=112
     export CONDA_PY=36
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
-    export CONDA_NPY=112
+    export CONDA_NPY=113
     export CONDA_PY=36
     set +x
     conda build /recipe_root --quiet || exit 1
